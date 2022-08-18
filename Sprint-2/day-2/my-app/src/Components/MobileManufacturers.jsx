03e7a8934f6822af0de2d855d34fc820a5eb4b0c
akram3598786 <akramali03598786@gmail.com>
2022-08-19 01:03:40 +0530

    Solution-1

---
+++ Sprint-2/day-2/my-app/src/Components/MobileManufacturers.jsx
@@ -0,0 +1,10 @@
+export default function Mobile_manuf (){
+    return (
+     <>
+       <li>Samsung</li>
+       <li>HTC</li>
+       <li>Micromax</li>
+       <li>Apple</li>
+     </>
+    );
+  }
---
+++ Sprint-2/day-2/my-app/src/Components/MobileOperSysytems.jsx
@@ -0,0 +1,11 @@
+
+export default function Mobile_OS(){
+  return (
+   <>
+     <li>Android</li>
+     <li>BlackBerry</li>
+     <li>iPhone-IOS</li>
+     <li>Windows-Phone</li>
+   </>
+  );
+}
