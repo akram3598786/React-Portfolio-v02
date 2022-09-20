03d285ba50dd754e090bdf7fb267aff6366a45a1
akram3598786 <akramali03598786@gmail.com>
2022-09-20 14:50:22 +0530

    resume and project updated

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -40,7 +40,7 @@ const Banner = () => {
               <button className="learn-more">
                 <a
                   href={
-                    "https://drive.google.com/file/d/1vV14PclUddc4S9fbZPF23xfS_J_mLZHg/view?usp=sharing"
+                    "https://drive.google.com/file/d/16NfQzxOqL7uok4AInTApqXd4vhCGDUHz/view?usp=sharing"
                   }
                   target={"_blank"}
                 >
